7961c22230705994c03a1573899e5f51322c46ca
Mathew Bramson <mathewbramson@gmail.com>
2017-04-22 11:30:04 -0400

    fix typo in config

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :mental_health_matters, MentalHealthMatters.Web.Endpoint,
   on_init: {MentalHealthMatters.Web.Endpoint, :load_from_system_env, []},
   url: [host: "mentalhealthmatters.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
