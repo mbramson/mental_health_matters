93553031a3402f18d2b834343aeaaf21f0c44b2d
Mathew Bramson <mathewbramson@gmail.com>
2017-04-22 11:29:00 -0400

    further config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,56 +15,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mental_health_matters, MentalHealthMatters.Web.Endpoint,
   on_init: {MentalHealthMatters.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
+  url: [host: "mentalhealthmatters.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :mental_health_matters, MentalHealthMatters.Web.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [:inet6,
-#               port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :mental_health_matters, MentalHealthMatters.Web.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :mental_health_matters, MentalHealthMatters.Web.Endpoint, server: true
-#
-
-config :mental_health_matters, MentalHealthMatters.Web.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :mental_health_matters, MentalHealthMatters.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  database: "mental_health_matters_prod",
-  pool_size: 15
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
